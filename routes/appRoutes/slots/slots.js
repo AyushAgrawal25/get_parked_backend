e7a1f423c09902f97df1845aaec67b675b8e5758
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2021-06-07 23:37:44 +0530

    Time stamp issue solved

--- routes/appRoutes/slots/slots.js
+++ routes/appRoutes/slots/slots.js
@@ -26,7 +26,7 @@ router.post("/create", tokenUtils.verify, async(req, res)=>{
                 vehiclesData.push(vehicleData);
             }
             
-            const vehicles=await prisma.slotVehicles.createMany({
+            const vehicles=await prisma.slotVehicle.createMany({
                 data:vehiclesData
             });
             if((vehicles)&&(vehicles.count>0)){
@@ -65,5 +65,68 @@ const slotCreateStatus={
 }
 
 // TODO: get parking lord function.
+router.get("/parkingLord", tokenUtils.verify, async(req, res)=>{
+    const userData=req.tokenData;
+    try {
+        let slot=await prisma.slot.findFirst({
+            where:{
+                userId:userData.id
+            },
+            include:{
+                vehicles:true,
+                SlotRatingReview:true
+            }
+        });
+
+        if(slot){
+            //TODO: Add Ratings..
+            const ratings=slot["SlotRatingReview"];
+            let totalRating=0;
+            let overallRating=null;
+            if(ratings.length>0){
+                ratings.forEach((rating)=>{
+                    totalRating+=rating.ratingValue;
+                });
+                overallRating=totalRating/ratings.length;
+            }
+            slot["SlotRatingReview"]=undefined;
+            slot["rating"]=overallRating;
+
+            res.statusCode=parkingLordGetStatus.success.code;
+            res.json({
+                message:parkingLordGetStatus.success.message,
+                data:slot,
+            });
+            return;
+        }
+        
+        res.statusCode=parkingLordGetStatus.notFound.code;
+        res.json({
+            message:parkingLordGetStatus.notFound.message
+        });
+    } catch (error) {
+        console.log(error);
+        res.statusCode=parkingLordGetStatus.serverError.code;
+        res.json({
+            error:error,
+            message:parkingLordGetStatus.serverError.message
+        });
+    }
+});
+
+const parkingLordGetStatus={
+    success:{
+        code:200,
+        message:"Parking Lord fetched sucessfully..."
+    },
+    notFound:{
+        code:404,
+        message:"No Parking Lord Found..."
+    },
+    serverError:{
+        code:500,
+        message:"Internal Server Error..."
+    }
+}
 
 module.exports = router;
