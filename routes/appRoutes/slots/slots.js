cf4f679e0a5d97884ea33d7d96f6cb9c99796e3d
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2021-06-12 00:16:15 +0530

    Minor Changes in Slot Data

--- routes/appRoutes/slots/slots.js
+++ routes/appRoutes/slots/slots.js
@@ -1,217 +1,238 @@
 const express = require('express');
-const crypto=require('crypto');
-const { PrismaClient } = require('@prisma/client');
+const crypto = require('crypto');
+const { PrismaClient, SlotSpaceType } = require('@prisma/client');
 
 const tokenUtils = require('./../../../services/tokenUtils/tokenUtils');
 const { parse } = require('dotenv');
+const vehiclesDetails = require('../../../services/vehicles/vehiclesDetails');
 
 const router = express.Router();
 const prisma = new PrismaClient();
 
-router.post("/create", tokenUtils.verify, async(req, res)=>{
+router.post("/create", tokenUtils.verify, async (req, res) => {
     const userData = req.tokenData;
-    try{
-        const userDetails=await prisma.userDetails.findFirst({
-            where:{
-                userId:parseInt(userData.id),
+    try {
+        const userDetails = await prisma.userDetails.findFirst({
+            where: {
+                userId: parseInt(userData.id),
             }
         });
-        if(!userDetails){
-            res.statusCode=slotCreateStatus.userDetailsNotFound.code;
+        if (!userDetails) {
+            res.statusCode = slotCreateStatus.userDetailsNotFound.code;
             res.json({
-                message:slotCreateStatus.userDetailsNotFound.message
+                message: slotCreateStatus.userDetailsNotFound.message
             });
             return;
         }
-        
-        let reqSlotData=req.body.slotData;
-        reqSlotData["status"]=1;
-        reqSlotData["token"]=crypto.randomBytes(40).toString('base64');
-        reqSlotData["userId"]=userData.id;    
-        const slot=await prisma.slot.create({
-            data:reqSlotData
+
+        let reqSlotData = req.body.slotData;
+        reqSlotData["spaceType"] = (reqSlotData["spaceType"] == 'Sheded') ? SlotSpaceType.Sheded : SlotSpaceType.Open;
+        reqSlotData["status"] = 1;
+        reqSlotData["token"] = crypto.randomBytes(40).toString('base64');
+        reqSlotData["userId"] = userData.id;
+        const slot = await prisma.slot.create({
+            data: reqSlotData
         });
-        if(slot){
-            let vehiclesData=[];
-            for(var i=0; i<req.body.vehicles.length; i++){
-                let vehicleData=req.body.vehicles[i];
-                vehicleData["slotId"]=slot.id;
-                vehicleData["status"]=1;
+        if (slot) {
+            let vehiclesData = [];
+            for (var i = 0; i < req.body.vehicles.length; i++) {
+                let vehicleData = req.body.vehicles[i];
+                vehicleData["slotId"] = slot.id;
+                vehicleData["status"] = 1;
                 vehiclesData.push(vehicleData);
             }
-            
-            const vehicles=await prisma.slotVehicle.createMany({
-                data:vehiclesData
+
+            const vehicles = await prisma.slotVehicle.createMany({
+                data: vehiclesData
             });
-            if((vehicles)&&(vehicles.count>0)){
-                res.statusCode=slotCreateStatus.success.code;
+            if ((vehicles) && (vehicles.count > 0)) {
+                res.statusCode = slotCreateStatus.success.code;
                 res.json({
-                    message:slotCreateStatus.success.message,
-                    slot:slot
+                    message: slotCreateStatus.success.message,
+                    slot: slot
                 });
             }
             return;
         }
-        res.statusCode=slotCreateStatus.serverError.code;
+        res.statusCode = slotCreateStatus.serverError.code;
         res.json({
-            message:slotCreateStatus.serverError.message,
+            message: slotCreateStatus.serverError.message,
         });
     }
-    catch(excp){
+    catch (excp) {
         console.log(excp);
-        res.statusCode=slotCreateStatus.serverError.code;
+        res.statusCode = slotCreateStatus.serverError.code;
         res.json({
-            message:slotCreateStatus.serverError.message,
-            error:excp
+            message: slotCreateStatus.serverError.message,
+            error: excp
         });
     }
 });
 
-const slotCreateStatus={
-    success:{
-        code:200,
-        message:"Slot Created successfully..."
+const slotCreateStatus = {
+    success: {
+        code: 200,
+        message: "Slot Created successfully..."
     },
-    userDetailsNotFound:{
-        code:400,
-        message:"User Details should be completed..."
+    userDetailsNotFound: {
+        code: 400,
+        message: "User Details should be completed..."
     },
-    serverError:{
-        code:500,
-        message:"Internal Server Error..."
+    serverError: {
+        code: 500,
+        message: "Internal Server Error..."
     }
 }
 
 // TODO: get parking lord function.
-router.get("/parkingLord", tokenUtils.verify, async(req, res)=>{
-    const userData=req.tokenData;
+router.get("/parkingLord", tokenUtils.verify, async (req, res) => {
+    const userData = req.tokenData;
     try {
-        let slot=await prisma.slot.findFirst({
-            where:{
-                userId:userData.id
+        let slot = await prisma.slot.findFirst({
+            where: {
+                userId: userData.id
             },
-            include:{
-                vehicles:true,
-                SlotRatingReview:true,
-                SlotImages:true
-            }
+            include: {
+                vehicles: true,
+                SlotImages: true
+            },
         });
 
-        if(slot){
+        if (slot){
             //TODO: Add Ratings..
-            const ratings=slot["SlotRatingReview"];
-            let totalRating=0;
-            let overallRating=null;
-            if(ratings.length>0){
-                ratings.forEach((rating)=>{
-                    totalRating+=rating.ratingValue;
-                });
-                overallRating=totalRating/ratings.length;
-            }
-            slot["SlotRatingReview"]=undefined;
-            slot["rating"]=overallRating;
+            const ratings = await prisma.slotRatingReview.aggregate({
+                _avg:{
+                    ratingValue:true,
+                },
+            });
 
-            slot["images"]=slot["SlotImages"];
-            slot["SlotImages"]=undefined;
+            slot["rating"] = ratings._avg.ratingValue;
 
-            res.statusCode=parkingLordGetStatus.success.code;
+            slot["images"] = slot["SlotImages"];
+            slot["SlotImages"] = undefined;
+            
+            // Important when u send vehicles data.
+            slot["vehicles"].forEach((vehicle)=>{
+                vehiclesDetails.parse(vehicle);
+            });
+            
+            res.statusCode = parkingLordGetStatus.success.code;
             res.json({
-                message:parkingLordGetStatus.success.message,
-                data:slot,
+                message: parkingLordGetStatus.success.message,
+                data: slot,
             });
             return;
         }
-        
-        res.statusCode=parkingLordGetStatus.notFound.code;
+
+        res.statusCode = parkingLordGetStatus.notFound.code;
         res.json({
-            message:parkingLordGetStatus.notFound.message
+            message: parkingLordGetStatus.notFound.message
         });
     } catch (error) {
         console.log(error);
-        res.statusCode=parkingLordGetStatus.serverError.code;
+        res.statusCode = parkingLordGetStatus.serverError.code;
         res.json({
-            error:error,
-            message:parkingLordGetStatus.serverError.message
+            error: error,
+            message: parkingLordGetStatus.serverError.message
         });
     }
 });
 
-const parkingLordGetStatus={
-    success:{
-        code:200,
-        message:"Parking Lord fetched sucessfully..."
+const parkingLordGetStatus = {
+    success: {
+        code: 200,
+        message: "Parking Lord fetched sucessfully..."
     },
-    notFound:{
-        code:400,
-        message:"No Parking Lord Found..."
+    notFound: {
+        code: 400,
+        message: "No Parking Lord Found..."
     },
-    serverError:{
-        code:500,
-        message:"Internal Server Error..."
+    serverError: {
+        code: 500,
+        message: "Internal Server Error..."
     }
 }
 
-router.put('/details', tokenUtils.verify, async(req, res)=>{
-    let userData=req.tokenData;
+router.put('/details', tokenUtils.verify, async (req, res) => {
+    let userData = req.tokenData;
     try {
-        const slot=await prisma.slot.findFirst({
-            where:{
-                userId:userData.id,
+        const slot = await prisma.slot.findFirst({
+            where: {
+                userId: userData.id,
             }
         });
-        if(!slot){
-            res.statusCode=slotDetailsUpdateStatus.notFound.code;
+        if (!slot) {
+            res.statusCode = slotDetailsUpdateStatus.notFound.code;
             res.json({
-                message:slotDetailsUpdateStatus.notFound.message
+                message: slotDetailsUpdateStatus.notFound.message
             });
             return;
         }
 
-        const slotUpdate=await prisma.slot.update({
-            data:{
-                name:req.body.name
+        const slotUpdate = await prisma.slot.update({
+            data: {
+                name: req.body.name
             },
-            where:{
-                id:slot.id
+            where: {
+                id: slot.id
             }
         });
 
-        if(slotUpdate){
-            res.statusCode=slotDetailsUpdateStatus.success.code;
+        if (slotUpdate) {
+            res.statusCode = slotDetailsUpdateStatus.success.code;
             res.json({
-                message:slotDetailsUpdateStatus.success.message,
-                data:slotUpdate
+                message: slotDetailsUpdateStatus.success.message,
+                data: slotUpdate
             });
             return;
         }
 
-        res.statusCode=slotDetailsUpdateStatus.serverError.code;
+        res.statusCode = slotDetailsUpdateStatus.serverError.code;
         res.json({
-            message:slotDetailsUpdateStatus.serverError.message
+            message: slotDetailsUpdateStatus.serverError.message
         });
     } catch (error) {
         console.log(error);
-        res.statusCode=slotDetailsUpdateStatus.serverError.code;
+        res.statusCode = slotDetailsUpdateStatus.serverError.code;
         res.json({
-            error:error,
-            message:slotDetailsUpdateStatus.serverError.message
+            error: error,
+            message: slotDetailsUpdateStatus.serverError.message
         });
     }
 });
 
-const slotDetailsUpdateStatus={
-    success:{
-        code:200,
+const slotDetailsUpdateStatus = {
+    success: {
+        code: 200,
         message: "Parking Lord Details Update Successfully..."
     },
-    notFound:{
-        code:400,
-        message:"Parking Lord not found..."
+    notFound: {
+        code: 400,
+        message: "Parking Lord not found..."
     },
-    serverError:{
-        code:500,
-        message:"Internal Server Error.."
+    serverError: {
+        code: 500,
+        message: "Internal Server Error.."
     }
 };
+
+// Complete this function.
+router.post('/parkingRequest', tokenUtils.verify, async (req, res) => {
+    const userData = req.tokenData;
+    try {
+        const parkingReq = await prisma.slotParkingRequest.create({
+            data: {
+                slotId: parseInt(req.body.slotId),
+                userId: parseInt(userData.id),
+                vehicleId: parseInt(req.body.vehicleId),
+                spaceType: parseInt(req.body.spaceType),
+                parkingHours: parseInt(req.body.spaceType),
+                status: 1
+            }
+        });
+    } catch (error) {
+
+    }
+});
 
 module.exports = router;
