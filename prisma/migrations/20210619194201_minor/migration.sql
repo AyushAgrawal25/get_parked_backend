877d98fd82be168ccdcd646ba2c8b1f2eb689aa4
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2021-06-26 13:43:28 +0530

    Preparing for server

--- prisma/migrations/20210619194201_minor/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `typeId` on table `slot_vehicles` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `slot_vehicles` MODIFY `typeId` INTEGER NOT NULL;
--- prisma/migrations/20210619195920_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `slot_vehicles` DROP FOREIGN KEY `slot_vehicles_ibfk_2`;
-
--- AddForeignKey
-ALTER TABLE `slot_vehicles` ADD FOREIGN KEY (`typeId`) REFERENCES `slot_vehicles_master`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
