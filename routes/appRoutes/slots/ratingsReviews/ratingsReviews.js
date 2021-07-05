25dcd4dd0b4a98701e1683ccdeb1f25c01dbdb96
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2021-07-05 13:41:05 +0530

    Rating Reviews Post Route is ready

--- routes/appRoutes/slots/ratingsReviews/ratingsReviews.js
+++ routes/appRoutes/slots/ratingsReviews/ratingsReviews.js
@@ -40,4 +40,91 @@ router.get("/:slotId", tokenUtils.verify, async(req, res)=>{
     }
 });
 
+router.post('/', tokenUtils.verify, async(req, res)=>{
+    const userData=req.tokenData;
+    try{
+        const parkingData=await prisma.slotParking.findUnique({
+            where:{
+                id:parseInt(req.body.parkingId)
+            },
+            include:{
+                slotRatingReview:true
+            }
+        });
+
+        if(!parkingData){
+            res.statusCode=ratingReviewPostStatus.unprocessableEntity.code;
+            res.json({
+                messsage:ratingReviewPostStatus.unprocessableEntity.messsage
+            });
+            return;
+        }
+
+        if(parkingData.status==1){
+            res.statusCode=ratingReviewPostStatus.unprocessableEntity.code;
+            res.json({
+                messsage:ratingReviewPostStatus.unprocessableEntity.messsage
+            });
+            return;
+        }
+
+        if(parkingData.slotRatingReview){
+            res.statusCode=ratingReviewPostStatus.unprocessableEntity.code;
+            res.json({
+                messsage:ratingReviewPostStatus.unprocessableEntity.messsage
+            });
+            return;
+        }
+        const ratingReviewCreate=await prisma.slotRatingReview.create({
+            data:{
+                slotId: parkingData.slotId,
+                parkingId:parkingData.id,
+                userId:parkingData.userId,
+                vehicleId:parkingData.vehicleId,
+                ratingValue:parseInt(req.body.ratingValue),
+                review:req.body.review,
+                status:1
+            }
+        });
+
+        if(!ratingReviewCreate){
+            res.statusCode=ratingReviewPostStatus.serverError.code;
+            res.json({
+                messsage:ratingReviewPostStatus.serverError.messsage
+            });
+            return;
+        }
+
+        res.statusCode=ratingReviewPostStatus.success.code;
+        res.json({
+            messsage:ratingReviewPostStatus.success.messsage,
+            data:ratingReviewCreate
+        });
+    }
+    catch(error){
+        console.log(error);
+        res.statusCode=ratingReviewPostStatus.serverError.code;
+        res.json({
+            messsage:ratingReviewPostStatus.serverError.messsage,
+            error:error
+        });
+        return;
+    }
+});
+
+const ratingReviewPostStatus={
+    success:{
+        code:200,
+        messsage:"Rating and Review successful..."
+    },
+    unprocessableEntity:{
+        code:422,
+        messsage:"This cannot be prcocessed..."
+    },
+    serverError:{
+        code:500,
+        messsage:"Internal Server Error...."
+    }
+}
+
 module.exports=router;
