14a6a7660a8914f804143c223af50a3b1cd0b9cc
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2021-07-15 00:13:00 +0530

    Change Time And Change Dimensions Route Added

--- routes/appRoutes/slots/slots.js
+++ routes/appRoutes/slots/slots.js
@@ -404,4 +404,202 @@ const slotDeactivateStatus={
     }
 }
 
+router.post('/changeTime', tokenUtils.verify, async(req, res)=>{
+    const userData = req.tokenData;
+    try {
+        const slotData=await prisma.slot.findFirst({
+            where:{
+                userId:parseInt(userData.id)
+            },
+            include:{
+                bookings:{
+                    where:{
+                        OR:[
+                            {
+                                status:1
+                            },
+                            {
+                                status:3
+                            }
+                        ]
+                    }
+                }
+            }
+        });
+
+        if(!slotData){
+            res.statusCode=slotChangeTimeStatus.notFound.code;
+            res.json({
+                message:slotChangeTimeStatus.notFound.message
+            });
+            return;
+        }
+
+        if(slotData.bookings.length>0){
+            res.statusCode=slotChangeTimeStatus.cannotBeChanged.code;
+            res.json({
+                message:slotChangeTimeStatus.cannotBeChanged.message,
+                data:slotData
+            });
+            return;
+        }
+
+        const slotTimeUpdate=await prisma.slot.update({
+            where:{
+                id:slotData.id
+            },
+            data:{
+                startTime:parseInt(req.body.startTime),
+                endTime:parseInt(req.body.endTime),
+            }
+        });
+
+        if(!slotTimeUpdate){
+            res.statusCode=slotChangeTimeStatus.serverError.code;
+            res.json({
+                message:slotChangeTimeStatus.serverError.message
+            });
+            return;
+        }
+        res.statusCode=slotChangeTimeStatus.success.code;
+        res.json({
+            message:slotChangeTimeStatus.success.message,
+            data:slotTimeUpdate
+        });
+    } catch (error) {
+        console.log(error);
+        res.statusCode=slotChangeTimeStatus.serverError.code;
+        res.json({
+            message:slotChangeTimeStatus.serverError.message
+        });
+        return;
+    }
+});
+
+const slotChangeTimeStatus={
+    success:{
+        code:200,
+        message:"Slot Times Changed Successfully..."
+    },
+    notFound:{
+        code:404,
+        message:"Slot not found..."
+    },
+    cannotBeChanged:{
+        code:422,
+        message:"Slot Timings Cannot be changed as some bookings or parkings are still there..."
+    },
+    serverError:{
+        code:500,
+        message:"Internal Server Error..."
+    }
+}
+
+router.post("/changeDimensions", tokenUtils.verify, async(req, res)=>{
+    const userData = req.tokenData;
+    try {
+        const slotData=await prisma.slot.findFirst({
+            where:{
+                userId:parseInt(userData.id)
+            },
+            include:{
+                vehicles:{
+                    where:{
+                        OR:[
+                            {
+                                status:1
+                            },
+                            {
+                                status:2
+                            },
+                        ]
+                    },
+                    include:{
+                        typeData:true
+                    }
+                },
+                bookings:{
+                    where:{
+                        OR:[
+                            {
+                                status:1
+                            },
+                            {
+                                status:3
+                            }
+                        ]
+                    }
+                }
+            }
+        });
+
+        if(!slotData){
+            res.statusCode=slotChangeTimeStatus.notFound.code;
+            res.json({
+                message:slotChangeTimeStatus.notFound.message
+            });
+            return;
+        }
+
+        if(slotData.bookings.length>0){
+            res.statusCode=slotChangeTimeStatus.cannotBeChanged.code;
+            res.json({
+                message:slotChangeTimeStatus.cannotBeChanged.message,
+                data:slotData
+            });
+            return;
+        }
+
+        // TODO: add slot logic here.
+        const nArea=parseFloat(req.body.length)*parseFloat(req.body.breadth);
+        const vehiclesUpdates=[];
+        slotData.vehicles.map((vehicleData)=>{
+            let vehicle=Object.assign({}, vehicleData);
+            if(vehicle.typeData.area>nArea){
+                vehicle.status=2;
+            }
+            else if(vehicle.typeData.height>parseFloat(req.body.height)){
+                vehicle.status=2;
+            }
+            else{
+                vehicle.status=1;
+            }
+
+            const vehicleId=vehicle.id;
+            vehicle.id=undefined;
+            vehicle.fair=undefined;
+            vehicle.slotId=undefined;
+            vehicle.type=undefined;
+            vehicle.typeData=undefined;
+            vehicle.typeId=undefined;
+
+            vehiclesUpdates.push({
+                where:{
+                    id:vehicleId
+                },
+                data:vehicle
+            });
+        });
+
+        const slotDimensionsUpdate=await prisma.slot.update({
+            where:{
+                id:slotData.id
+            },
+            data:{
+                vehicles:{
+                    update:vehiclesUpdates
+                },
+                height:parseFloat(req.body.height),
+                breadth:parseFloat(req.body.breadth),
+                length:parseFloat(req.body.length)
+            }
+        });
+
+        res.json(slotDimensionsUpdate);
+    } catch (error) {
+        console.log(error);
+        res.json(error);
+    }
+});
+
 module.exports = router;
